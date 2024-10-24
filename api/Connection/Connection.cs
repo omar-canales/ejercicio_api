ad975183d000dfd4d35c8b87d270a41c8551626a
Omar Canales <omar.canales@outlook.com>
2024-10-24 00:13:04 -0600

    Agregar archivos de proyecto.

---
+++ api/Connection/Connection.cs
@@ -0,0 +1,36 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace api.Connection
+{
+    public class Connection : IConnection
+    {
+        private readonly IConfiguration _configuration;
+
+        public Connection(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IDbConnection GetConnection
+        {
+            get
+            {
+                var sqlConnection = new SqlConnection();
+                if (sqlConnection == null)
+                {
+                    return null;
+                }
+
+                sqlConnection.ConnectionString = _configuration.GetConnectionString("SQL");
+                sqlConnection.Open();
+                return sqlConnection;
+            }
+        }
+    }
+
+    public interface IConnection
+    {
+        IDbConnection GetConnection { get; }
+    }
+}
---
+++ api/Services/IUsuarios.cs
@@ -0,0 +1,12 @@
+﻿using api.DTO;
+
+namespace api.Services
+{
+    public interface IUsuarios
+    {
+        Task<int> AddAsync(UsuarioDTO usuario);
+        Task<IEnumerable<UsuarioDTO>> GetAllAsync();
+        Task<UsuarioDTO> GetByIdAsync(int id);
+        string Usuario();
+    }
+}
